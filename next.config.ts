870db763247360bde071797d322ae980e24719b0
JocaBadasss <joaocarlos1208@hotmail.com>
2025-07-16 11:31:31 -0300

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   images: {
-    domains:
-      process.env.NODE_ENV === 'production'
-        ? ['oficina-demo.fly.dev']
-        : ['localhost', '127.0.0.1'],
+    domains: isProd ? ['api.rechcaminhoes.com.br'] : ['localhost', '127.0.0.1'],
   },
-
 };
 
 export default nextConfig;
